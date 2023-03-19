8271e07ab23095f1860ac7fd2635df4e8f17e86d
Gennady Zolotukhin <g.a.zolotukhin@gmail.com>
2023-03-19 22:59:34 +0300

    Add Book

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/')]
+    public function root(): Response
+    {
+        return $this->json(['test' => 'hello']);
+    }
+}
