45bbcd1119fe69cb201cf3a2ee8562f5cabf0f3e
Gennady Zolotukhin <g.a.zolotukhin@gmail.com>
2023-03-23 09:51:39 +0300

    Add Book (2)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    public function __construct(private BookRepository $bookRepository)
+    {
+    }
+
     #[Route('/')]
     public function root(): Response
     {
-        return $this->json(['test' => 'hello']);
+        $books = $this->bookRepository->findAll();
+
+        return $this->json($books);
     }
 }
